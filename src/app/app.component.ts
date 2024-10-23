81ccc0a9fc745e71681fb144d3ab1e114bc9d956
lespuch-v <v.lespukh@gmail.com>
2024-10-24 00:42:45 +0200

    - bug fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
-import { RouterTestingModule } from '@angular/router/testing';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuComponent, RouterTestingModule],
+  imports: [RouterOutlet, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
